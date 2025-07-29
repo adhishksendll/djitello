5f5953722c21b7707d2916e6182254af92af6edf
adhishksendll <adhishksenthil@gmail.com>
2025-07-29 15:35:07 +0530

    Mapping-Beta.py

--- Mapping.py
+++ Mapping.py
@@ -1,5 +1,3 @@
-# main.py
-
 from djitellopy import Tello
 import KeyPressModule as kp
 from time import sleep
@@ -24,38 +22,33 @@ me = Tello()
 me.connect()
 print("Battery:", me.get_battery())
 
-points = [(x, y)]  # Start point
+points = [(x, y)]  # Path points
 
 def getKeyboardInput():
     lr, fb, ud, yw = 0, 0, 0, 0
-    speed = 50
+    speed = 15
     aspeed = 50
     global x, y, yaw
 
     d = 0
+    angle = None
 
     if kp.getKey("LEFT"):
         lr = -speed
         d = dInterval
         angle = -90
-
     elif kp.getKey("RIGHT"):
         lr = speed
         d = dInterval
         angle = 90
-
     elif kp.getKey("UP"):
         fb = speed
         d = dInterval
         angle = 0
-
     elif kp.getKey("DOWN"):
         fb = -speed
         d = dInterval
         angle = 180
-
-    else:
-        angle = None
 
     if kp.getKey("w"):
         ud = speed
@@ -74,6 +67,8 @@ def getKeyboardInput():
     elif kp.getKey("q"):
         me.land()
 
+    yaw %= 360  # Keep yaw in 0–359 degrees
+
     if angle is not None:
         x += int(d * math.cos(math.radians(yaw + angle)))
         y += int(d * math.sin(math.radians(yaw + angle)))
@@ -81,22 +76,37 @@ def getKeyboardInput():
     return [lr, fb, ud, yw, x, y]
 
 def drawPoints(img, points):
-    for point in points:
-        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
-    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED)
-    cv2.putText(img, f'({(points[-1][0]-500)/100:.1f}, {(points[-1][1]-500)/100:.1f})m',
-                (points[-1][0]+10, points[-1][1]+30),
-                cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
+    for i in range(1, len(points)):
+        cv2.line(img, points[i - 1], points[i], (255, 255, 0), 2)
+        cv2.circle(img, points[i], 4, (0, 0, 255), cv2.FILLED)
+
+    cv2.circle(img, points[-1], 6, (0, 255, 0), cv2.FILLED)
+    coords = f'({(points[-1][0] - 500) / 100:.1f}, {(points[-1][1] - 500) / 100:.1f}) m'
+    cv2.putText(img, coords, (points[-1][0] + 10, points[-1][1] + 30),
+                cv2.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 255), 1)
+
+def drawBattery(img, battery):
+    cv2.putText(img, f'Battery: {battery}%', (10, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
 while True:
     vals = getKeyboardInput()
     me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
     img = np.zeros((1000, 1000, 3), np.uint8)
+
     if points[-1][0] != vals[4] or points[-1][1] != vals[5]:
         points.append((vals[4], vals[5]))
 
     drawPoints(img, points)
-    cv2.imshow("Path Visualization", img)
-    cv2.waitKey(1)
+    drawBattery(img, me.get_battery())
+
+    cv2.imshow("Tello Path Visualization", img)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
+        me.land()
+        break
+
     sleep(interval)
+
+cv2.destroyAllWindows()
